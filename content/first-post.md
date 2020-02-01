f6602ddedc4ed522ede67ab7edd789a8ae3e6d29
Ambar Mehrotra <mehrotraambar@gmail.com>
2020-02-01 05:41:34 +0530

    first commit

---
+++ content/first-post.md
@@ -0,0 +1,25 @@
+Title: First Post
+Date: 2010-12-03 10:20
+Modified: 2010-12-05 19:30
+Category: Python
+Tags: pelican, publishing
+Slug: my-super-post
+Authors: Ambar Mehrotra
+Summary: Short version for index and feeds
+
+Name | Lunch order | Spicy      | Owes
+------- | ---------------- | ---------- | ---------:
+Joan  | saag paneer | medium | $11
+Sally  | vindaloo        | mild       | $14
+Erin   | lamb madras | HOT      | $5
+
+There are **multiple syntax highlighting themes** to choose from. Here's one of them:
+
+```javascript
+// All the code you will ever need
+var hw = "Hello World!"
+alert(hw);
+```
+
+My math is so rusty that I barely remember the _quadratic equation_:
+$-b \pm \sqrt{b^2 - 4ac} \over 2a$
---
+++ pelicanconf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Ambar Mehrotra'
+SITENAME = 'All Things Connected'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Calcutta'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+AUTHOR_FEED_RSS = 'feeds/%s.rss.xml'
+RSS_FEED_SUMMARY_ONLY = False
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+THEME = "D:\workspace\pelican\pelican-themes\\hyde"
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
